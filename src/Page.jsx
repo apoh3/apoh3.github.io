c1f6dedd0b7ccb26103b41d44072014acde8001e
apoh3 <pohallison@gmail.com>
2025-08-27 13:55:51 -0400

    update nav links

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 function About() {
-  return <h2>This site is temporarily underconstruction</h2>;
+  return <h2>This is home</h2>;
 }
 
 function Publications() {
@@ -24,6 +24,7 @@ function Page() {
   return (
     <main>
       <Routes>
+        <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/publications" element={<Publications />} />
         <Route path="/teaching" element={<Teaching />} />
