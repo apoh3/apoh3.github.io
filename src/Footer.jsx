c1f93042b19e9092f79cd2a2212d5d86b63b4c1c
apoh3 <pohallison@gmail.com>
2025-08-29 15:17:36 -0400

    completed Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,35 @@
-import { Mail } from "lucide-react";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-links">
+        <a
+          href="mailto:apoh@umass.edu"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Email"
+        >
+          <MdEmail className="icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/allisonpoh"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="LinkedIn"
+        >
+          <FaLinkedin className="icon" />
+        </a>
+        <a
+          href="https://scholar.google.com/citations?hl=en&user=r4sloMMAAAAJ"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Google Scholar"
+        >
+          <i className="ai ai-google-scholar icon"></i>
+        </a>
+      </div>
       <p>Last updated: August 2025</p>
       <p>© {new Date().getFullYear()} Allison Poh</p>
     </footer>
